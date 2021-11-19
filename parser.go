59c58a9093010068bde17f1f5e61e5ad65e73986
aquilax <aquilax@gmail.com>
2021-11-19 11:21:33 +0100

    Parses comments and metadata happy paths

--- parser.go
+++ parser.go
@@ -1,8 +1,16 @@
 package cooklang
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
 
-type Parser struct{}
+const (
+	COMMENTS_LINE_PREFIX     = "--"
+	METADATA_LINE_PREFIX     = ">>"
+	METADATA_VALUE_SEPARATOR = ":"
+)
 
 type Equipment struct {
 	Name string
@@ -31,19 +39,74 @@ type Step struct {
 	Comments    string
 }
 
+type Metadata = map[string]string
+
 type Recipe struct {
 	Steps    []Step
-	Metadata map[string]string
-}
-
-func NewParser() *Parser {
-	return &Parser{}
+	Metadata Metadata
 }
 
 func ParseString(s string) (*Recipe, error) {
 	if s == "" {
 		return nil, fmt.Errorf("Recipe string must not be empty")
 	}
+
 	// TODO parse recipe
-	return nil, nil
+	scanner := bufio.NewScanner(strings.NewReader(s))
+	recipe := Recipe{
+		make([]Step, 0),
+		make(map[string]string),
+	}
+	var line string
+	for scanner.Scan() {
+		line = scanner.Text()
+		if line != "" {
+			err := parseLine(line, &recipe)
+			if err != nil {
+				return nil, err
+			}
+		}
+		fmt.Println(scanner.Text())
+	}
+	return &recipe, nil
+}
+
+func parseLine(line string, recipe *Recipe) error {
+	if strings.HasPrefix(line, COMMENTS_LINE_PREFIX) {
+		commentLine, err := parseSingleLineComment(line)
+		if err != nil {
+			return err
+		}
+		recipe.Steps = append(recipe.Steps, Step{Comments: commentLine})
+	} else if strings.HasPrefix(line, METADATA_LINE_PREFIX) {
+		key, value, err := parseMetadata(line)
+		if err != nil {
+			return err
+		}
+		recipe.Metadata[key] = value
+	} else {
+		step, err := parseRecipe(line)
+		if err != nil {
+			return err
+		}
+		recipe.Steps = append(recipe.Steps, *step)
+	}
+	return nil
+}
+
+func parseSingleLineComment(line string) (string, error) {
+	return strings.TrimSpace(line[2:]), nil
+}
+
+func parseMetadata(line string) (string, string, error) {
+	metadataLine := strings.TrimSpace(line[2:])
+	index := strings.Index(metadataLine, METADATA_VALUE_SEPARATOR)
+	if index < 1 {
+		return "", "", fmt.Errorf("invalid metadata: %s", metadataLine)
+	}
+	return strings.TrimSpace(metadataLine[:index]), strings.TrimSpace(metadataLine[index+1:]), nil
+}
+
+func parseRecipe(line string) (*Step, error) {
+	return &Step{}, nil
 }
--- parser_test.go
+++ parser_test.go
@@ -18,6 +18,30 @@ func TestParseString(t *testing.T) {
 			nil,
 			true,
 		},
+		{
+			"Parses single line comments",
+			"--This is a comment",
+			&Recipe{
+				Steps: []Step{
+					{
+						Comments: "This is a comment",
+					},
+				},
+				Metadata: make(Metadata),
+			},
+			false,
+		},
+		{
+			"Parses metadata",
+			">> key: value",
+			&Recipe{
+				Steps: []Step{},
+				Metadata: Metadata{
+					"key": "value",
+				},
+			},
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
